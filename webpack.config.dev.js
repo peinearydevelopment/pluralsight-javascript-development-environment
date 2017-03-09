c497e6266d5a395dd531a50cc2ed58f7450300be
PeinearyDevelopment <peinearydevelopment@gmail.com>
2017-03-09 15:15:38 -0500

    bundling module

---
+++ webpack.config.dev.js
@@ -0,0 +1,30 @@
+import path from 'path';
+
+export default {
+  debug: true,
+  devtool: 'inline-source-map', // determines what level of output to be contained in transpiled files
+  noInfo: false, // whether or not to display files transpiled in the command line window
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'), // this is actually just creating files in memory, which is why the output path is 'src'
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [],
+  module: {
+    loaders: [
+      {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loaders: ['babel']
+      },
+      {
+          test: /\.css$/,
+          loaders: ['style','css']
+      }
+    ]
+  }
+}
